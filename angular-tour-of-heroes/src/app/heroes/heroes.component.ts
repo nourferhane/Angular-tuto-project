bd0b880dea82e923c646233693a2c91e5d4015c4
Nough <n>
2021-04-11 23:26:32 +0000

    Add : interface + twoway binding + pipe

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../hero';
+import {FormsModule} from '@angular/forms';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit {
 
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id : 1,
+    name :'Windstrom'
+  };
   constructor() { }
 
   ngOnInit(): void {
