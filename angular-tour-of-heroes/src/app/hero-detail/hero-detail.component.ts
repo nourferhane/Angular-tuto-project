115048aafff8b5d09cebf5bd77422de823e80a3a
Nough <n>
2021-04-14 21:57:21 +0000

    add service

---
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,15 @@
+import {Hero} from '../hero'
+import { Component, OnInit, Input } from '@angular/core';
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero? : Hero;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
